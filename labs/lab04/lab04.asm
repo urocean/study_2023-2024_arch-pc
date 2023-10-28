42850c8a94995c901265cd9936ef9c8cc9cd43dc
Осина Виктория <uroocean@gmail.com>
2023-10-28 23:01:23 +0300

    Added hello.asm and lab04.asm for lab04

---
+++ labs/lab04/lab04.asm
@@ -0,0 +1,18 @@
+; lab04.asm
+SECTION .data ; Начало секции данных
+	lab04: DB 'Осина Виктория',10 
+	lab04Len: EQU $-lab04 ; Длина строки lab04
+	
+SECTION .text ; Начало секции кода
+	GLOBAL _start
+	
+_start: ; Точка входа в программу
+	mov eax,4 ; Системный вызов для записи (sys_write)
+	mov ebx,1 ; Описатель файла '1' - стандартный вывод
+	mov ecx,lab04 ; Адрес строки lab04 в ecx
+	mov edx,lab04Len ; Размер строки hello
+	int 80h ; Вызов ядра
+	
+	mov eax,1 ; Системный вызов для выхода (sys_exit)
+	mov ebx,0 ; Выход с кодом возврата '0' (без ошибок)
+	int 80h ; Вызов ядра
